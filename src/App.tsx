209e1a86cf49bd3bc13a4e29fd7fcbc7c3724d82
martins20 <martins20.tech@gmail.com>
2021-03-21 10:14:00 -0300

    Chore: Initial Commit
    - [x] Implement styled component.
    - [x] Create a simple react app with Typescript template.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>App Component</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
